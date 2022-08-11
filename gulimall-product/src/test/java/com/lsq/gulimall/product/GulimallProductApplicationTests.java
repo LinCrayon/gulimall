9fc81de3914f651da776fb3515ac61f3e394f363
Tame-complexity <linshengqian@outlook.com>
2022-08-11 16:08:09 +0800

    初始化完成

---
+++ gulimall-product/src/test/java/com/lsq/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsq.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
